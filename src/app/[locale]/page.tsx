5bf0ae6e285ec87798506400932ab322721f9591
lazakrisz <lezer11@icloud.com>
2025-07-04 14:17:42 +0200

    add links to homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,33 @@
 import { Link } from "@/i18n/navigation";
 import { useTranslations } from "next-intl";
+import NextLink from "next/link";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
 
   return (
-    <div>
-      <h1>{t("title")}</h1>
+    <div className="max-w-screen-lg mx-auto py-12">
+      <h1 className="text-2xl font-bold">{t("title")}</h1>
 
-      <Link href="/news">News</Link>
+      <nav className="mt-4">
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/news">
+              News{" "}
+              <span className="text-sm text-gray-500">i18n, middleware</span>
+            </Link>
+          </li>
+
+          <li>
+            <NextLink href="/test">
+              Test{" "}
+              <span className="text-sm text-gray-500">
+                No i18n, no middleware
+              </span>
+            </NextLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
